9b6ee11d72acb766253cf129eb2054767c1d9b83
Pushkar Khadilkar <pkhadilkar@gmail.com>
2015-02-20 21:44:23 +0530

    Added UsAcO cowtour

--- usaco/cowtour/cowtour.cpp
+++ usaco/cowtour/cowtour.cpp
@@ -33,7 +33,7 @@ double compute_distance(int i, int j) {
 
 
 void pasture_of(int l) {
-	cout << "computing pasture of " << l << endl;
+	 //cout << "computing pasture of " << l << endl;
 	leader[l] = l;
 	set<int> * s = new set<int>();
 	queue<int> q;
@@ -47,15 +47,16 @@ void pasture_of(int l) {
 				q.push(i);
 				s->insert(i);
 				leader[i] = l;
+				 //cout << i << " ";
 			}
 		}
 	}
-
-    pastures[l]= s;
+	//cout << endl;
+    	pastures[l]= s;
 }
 
 void precompute() {
-    
+
     // compute all pairs shortest paths
 	FOR(k, 0, N) {
 		FOR(i, 0, N) {
@@ -67,7 +68,15 @@ void precompute() {
 			}
 		}
 	}
-    
+
+	/*FOR(i, 0, N) {
+		FOR(j, 0, N) {
+			 cout << dist[i][j] << " ";
+		}
+		cout << endl;
+	}*/
+
+
 	fill(leader, leader + N, -1);
 	FOR(i, 0, N) {
 		if(leader[i] == -1) {
@@ -79,13 +88,40 @@ void precompute() {
 // p1 and p2 are now connected find new diameter
 void find_diameter(set<int> *f1, set<int> * f2, int p1, int p2, double edge_distance) {
     double d = 0;
-    for(SI xt = f1->begin(); xt != f1->end(); xt++) {
+
+    for(SI xt = f1->begin(); xt != f1->end(); ++xt) {
         for(SI yt = f2->begin(); yt != f2->end(); ++yt) {
             double tmp = dist[*xt][p1] + dist[p2][*yt] + edge_distance;
-            if(d < tmp)    d = tmp;
+            if(d < tmp)    {
+		//cout << "d1 :" << d << "xt: " << *xt  << " yt: " << *yt << endl;
+		d = tmp;
+	    }
         }
     }
+
+    set<int> * s = 0;
+
+    if(f1->size() == 1 && f2->size() > 1) {
+	s = f2;
+    } else if(f2->size() == 1 && f1->size() > 1) {
+	s = f1;
+    }
+
+    if(s != 0) {
+    	for(SI xt = s->begin(); xt != s->end(); ++xt) {
+        	for(SI yt = s->begin(); yt != s->end(); ++yt) {
+	            double tmp = dist[*xt][*yt];
+        		if(d < tmp)    {
+				//cout << "d2 :" << d << "xt: " << *xt  << " yt: " << *yt << endl;
+				d = tmp;
+	    		}
+        	}
+    	}
+    }
+
+    /* cout << "D: " << D<< "\n"; */
     if(D > d)    D = d;
+    //cout << "After D: " << D << "\n";
 }
 
 int main() {
@@ -104,9 +140,9 @@ int main() {
 			adj[i][j] = (s[j] == '1');
 			if(adj[i][j] && i < j) {
 				dist[i][j] = dist[j][i] = compute_distance(i, j);
-			} /*else if(dist[i][j] == 0){
+			}else if(dist[i][j] == 0 && i != j){
 				dist[i][j] = MAX_DISTANCE;
-			}*/
+			}
 		}
 	}
 
@@ -117,8 +153,8 @@ int main() {
 	FOR(i, 0, N) {
 		FOR(j, i + 1, N) {
 			if(!adj[i][j] && leader[i] != leader[j]) {
-				cout << "i: " << i << " j: " << j << endl;
-				
+				 //cout << "i: " << i << " j: " << j << endl;
+
 				set<int> *s_i = pastures[leader[i]];
 				set<int> *s_j = pastures[leader[j]];
 	            find_diameter(s_i, s_j, i, j, compute_distance(i, j));
